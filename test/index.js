cb205135f9578ac2583af4d64718d6be18403423
Aperçu <bgronon@gmail.com>
2016-04-16 14:52:24 +0200

    refactor(lint): switch to eslint

--- test/index.js
+++ test/index.js
@@ -1,29 +1,28 @@
+'use strict';
 
 var Test = require('segmentio-integration-tester');
-var helpers = require('./helpers');
-var facade = require('segmentio-facade');
 var hash = require('string-hash');
-var mapper = require('../lib/mapper');
 var time = require('unix-time');
 var should = require('should');
-var assert = require('assert');
-var Intercom = require('..');
 var redis = require('redis');
+var helpers = require('./helpers');
+var mapper = require('../lib/mapper');
+var Intercom = require('..');
 
-describe('Intercom', function(){
+describe('Intercom', function() {
   var intercom;
   var settings;
   var payload;
   var test;
   var db;
 
-  before(function(done){
+  before(function(done) {
     db = redis.createClient();
     db.on('ready', done);
     db.on('error', done);
   });
 
-  beforeEach(function(){
+  beforeEach(function() {
     payload = {};
     settings = {
       appId: 'a3vy8ufv',
@@ -35,7 +34,7 @@ describe('Intercom', function(){
     intercom.redis(db);
   });
 
-  it('should have correct settings', function(){
+  it('should have correct settings', function() {
     test
       .name('Intercom')
       .endpoint('https://api-segment.intercom.io')
@@ -44,93 +43,92 @@ describe('Intercom', function(){
       .channels(['server']);
   });
 
-  describe('.validate()', function(){
-
-    it('should be invalid if .appId is missing', function(){
+  describe('.validate()', function() {
+    it('should be invalid if .appId is missing', function() {
       delete settings.appId;
       test.invalid({}, settings);
     });
 
-    it('should be invalid if .apiKey is missing', function(){
+    it('should be invalid if .apiKey is missing', function() {
       delete settings.apiKey;
       test.invalid({}, settings);
     });
 
-    it('should be invalid if .userId and .email are missing', function(){
+    it('should be invalid if .userId and .email are missing', function() {
       test.invalid({}, settings);
     });
 
-    it('should be valid when just .userId is given', function(){
+    it('should be valid when just .userId is given', function() {
       test.valid({ userId: '12345' }, settings);
     });
 
-    it('should be valid when just .email is given', function(){
+    it('should be valid when just .email is given', function() {
       test.valid({ properties: { email: 'foo@bar.com' } }, settings);
     });
 
-    it('should be valid when .apiKey and .appId are given', function(){
+    it('should be valid when .apiKey and .appId are given', function() {
       test.valid({ userId: '12345' }, settings);
     });
   });
 
-  describe('mapper', function(){
-    describe('identify', function(){
-      it('should map basic identify', function(){
+  describe('mapper', function() {
+    describe('identify', function() {
+      it('should map basic identify', function() {
         test.maps('identify-basic');
       });
 
-      it('should map basic context', function(){
+      it('should map basic context', function() {
         test.maps('identify-context');
       });
 
-      it('should respect .active()', function(){
+      it('should respect .active()', function() {
         test.maps('identify-active');
       });
 
-      it('should map nested dates', function(){
+      it('should map nested dates', function() {
         test.maps('identify-nested-dates');
       });
 
-      it('should map companies', function(){
+      it('should map companies', function() {
         test.maps('identify-companies');
       });
 
-      it('should map a company', function(){
+      it('should map a company', function() {
         test.maps('identify-company');
       });
 
-      it('should map companies with remove', function(){
+      it('should map companies with remove', function() {
         test.maps('identify-companies-remove');
       });
 
-      it('should map a company with remove', function(){
+      it('should map a company with remove', function() {
         test.maps('identify-company-remove');
       });
 
-      it('should update last_request_at with lastRequestAt when supplied', function(){
+      it('should update last_request_at with lastRequestAt when supplied', function() {
         test.maps('identify-last-request-at');
       });
 
-      it('should update unsubscribed_from_emails with unsubscribedFromEmails when supplied', function(){
+      it('should update unsubscribed_from_emails with unsubscribedFromEmails when supplied', function() {
         test.maps('identify-unsubscribed-from-emails');
       });
     });
 
-    describe('group', function(){
-      it('should map basic group', function(){
+    describe('group', function() {
+      it('should map basic group', function() {
         test.maps('group-basic');
       });
     });
 
-    describe('track', function(){
-      it('should map basic track', function(){
+    describe('track', function() {
+      it('should map basic track', function() {
         test.maps('track-basic');
       });
     });
   });
 
-  describe('.identify()', function(){
-    it('should be able to identify correctly', function(done){
+  describe('.identify()', function() {
+    it('should be able to identify correctly', function(done) {
       var msg = helpers.identify();
 
       payload.user_id = msg.userId();
@@ -154,15 +152,15 @@ describe('Intercom', function(){
         .end(done);
     });
 
-    it('should not error on invalid companies', function(done){
-      var identify = helpers.identify({ traits: { companies: 'foo' }});
-      intercom.identify(identify, function(err){
+    it('should not error on invalid companies', function(done) {
+      var identify = helpers.identify({ traits: { companies: 'foo' } });
+      intercom.identify(identify, function(err) {
         should.not.exist(err);
         done();
       });
     });
 
-    it('should send the ip address', function(done){
+    it('should send the ip address', function(done) {
       var timestamp = new Date();
       test
         .set(settings)
@@ -175,12 +173,12 @@ describe('Intercom', function(){
           custom_attributes: { id: 'userId' },
           last_request_at: time(timestamp),
           last_seen_ip: '70.211.71.236',
-          user_id: 'userId',
+          user_id: 'userId'
         })
         .expects(200, done);
     });
 
-    it('should error on invalid creds', function(done){
+    it('should error on invalid creds', function(done) {
       test
         .set({ apiKey: 'x' })
         .identify({})
@@ -188,8 +186,8 @@ describe('Intercom', function(){
     });
   });
 
-  describe('.group()', function(){
-    it('should be able to group correctly', function(done){
+  describe('.group()', function() {
+    it('should be able to group correctly', function(done) {
       var group = test.fixture('group-basic');
       test.group(group.input);
       test.requests(2);
@@ -223,24 +221,24 @@ describe('Intercom', function(){
         .expects(200);
 
       test.end(done);
-    })
+    });
 
-    it('should work with .created_at', function(done){
+    it('should work with .created_at', function(done) {
       var traits = { created_at: 'Jan 1, 2000 3:32:33 PM', name: 'old company' };
       var group = helpers.group({ traits: traits, groupId: 'a5322d6' });
       delete group.obj.traits.created;
       intercom.group(group, done);
-    })
+    });
 
-    it('should work with .created', function(done){
+    it('should work with .created', function(done) {
       var traits = { created: 'Jan 1, 2014 3:32:33 PM', name: 'new company' };
       var group = helpers.group({ traits: traits, groupId: 'e186e5de' });
       intercom.group(group, done);
-    })
-  })
+    });
+  });
 
-  describe('.track()', function(){
-    it('should track', function(done){
+  describe('.track()', function() {
+    it('should track', function(done) {
       var json = test.fixture('track-basic');
       test
         .set(settings)
@@ -250,7 +248,7 @@ describe('Intercom', function(){
         .end(done);
     });
 
-    it('should error on invalid creds', function(done){
+    it('should error on invalid creds', function(done) {
       test
         .set({ apiKey: 'x' })
         .track({})
